d681658cab5bf956b400d09e2eb75d2d18184ba6
s3986423 <s3986423@rmit.edu.vn>
2023-09-19 13:09:29 +0700

    All parent class commit (System)
    Commit the class Container, Trip, Vehicles with all initial attribute for Vehicles (may change later)

---
+++ Vehicles.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Vehicles {
+    int VehicleID;
+    String name;
+    double currentFuel;
+    double carryingCapacity;
+    double fuelCapacity;
+    Port currentPort;
+    ArrayList<Container> numContainer;
+    ArrayList<Trip> trip;
+}
