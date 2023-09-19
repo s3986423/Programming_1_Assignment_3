6c4e9b807d76c8815f9d8a09c009458f945ec67f
khoabronze <s3986281@rmit.edu.vn>
2023-09-19 17:13:25 +0700

    Update Port.java

--- Port.java
+++ Port.java
@@ -14,19 +14,6 @@ public class Port {
 
     public Port() {}
 
-    public Port(int portID, String name, double latitude, double longititude, double storingCapacity, boolean landingAbility, PortManager portManager, ArrayList<Container> containers, ArrayList<Vehicles> vehicles, ArrayList<Trip> trafficHistory) {
-        this.portID = portID;
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longititude;
-        this.storingCapacity = storingCapacity;
-        this.landingAbility = landingAbility;
-        this.portManager = portManager;
-        this.containers = containers;
-        this.vehicles = vehicles;
-        this.trafficHistory = trafficHistory;
-    }
-
     public int getPortID() {
         return portID;
     }
@@ -106,6 +93,21 @@ public class Port {
     public void setTrafficHistory(ArrayList<Trip> trafficHistory) {
         this.trafficHistory = trafficHistory;
     }
+
+    public Port(int portID, String name, double latitude, double longititude, double storingCapacity, boolean landingAbility, PortManager portManager, ArrayList<Container> containers, ArrayList<Vehicles> vehicles, ArrayList<Trip> trafficHistory) {
+        this.portID = portID;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longititude;
+        this.storingCapacity = storingCapacity;
+        this.landingAbility = landingAbility;
+        this.portManager = portManager;
+        this.containers = containers;
+        this.vehicles = vehicles;
+        this.trafficHistory = trafficHistory;
+    }
+
+
 
     public double calDistance(Port port) {
 
