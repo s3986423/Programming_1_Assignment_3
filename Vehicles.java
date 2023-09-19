1886c9dd926174de4b0b4dd4a177996c5dce9afe
s3986423 <s3986423@rmit.edu.vn>
2023-09-19 21:30:42 +0700

    Update Vehicles.java
    load method

--- Vehicles.java
+++ Vehicles.java
@@ -110,11 +110,12 @@ class Ship extends Vehicles{
     }
 
     // Load(Container) (Ship)
-
     @Override
     public void load(Container container) {
         if (this.calCurrentCapacity() + container.getWeight() > this.getCarryingCapacity()) {
             System.out.println("The vehicle does not have enough capacity for this container");
+        } else if (this.getCurrentPort() == null) {
+            System.out.println("The vehicle is moving and can not load");
         }
         else {
             this.getCurrentPort().removeContainer(container);
