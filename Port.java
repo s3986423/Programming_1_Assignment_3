fbee97d09594ce4b659167249a52ebe48a2bbc05
khoabronze <s3986281@rmit.edu.vn>
2023-09-19 13:14:40 +0700

    Update Port.java

--- Port.java
+++ Port.java
@@ -1,4 +1,15 @@
-public class Port {
+import java.util.List;
 
+public class Port {
+    private int portID;
+    private String name;
+    private double latitude;
+    private double storingCapacity;
+    private boolean landingAbility;
+    private PortManager portManager;
+    private List<Container> containers;
+    private List<Vehicles> vehicles;
+    private List<Trip> trafficHistory;
+    
 }
 
