c7e957411491785f3264fcdb59f94ad5dbe61240
khoabronze <s3986281@rmit.edu.vn>
2023-09-19 15:07:23 +0700

    Update Port.java

--- Port.java
+++ Port.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Port {
     private int portID;
+
     private String name;
     private double latitude;
     private double longititude;
@@ -12,5 +13,17 @@ public class Port {
     private ArrayList<Vehicles> vehicles;
     private ArrayList<Trip> trafficHistory;
 
+    public Port(int portID, String name, double latitude, double longititude, double storingCapacity, boolean landingAbility, PortManager portManager, ArrayList<Container> containers, ArrayList<Vehicles> vehicles, ArrayList<Trip> trafficHistory) {
+        this.portID = portID;
+        this.name = name;
+        this.latitude = latitude;
+        this.longititude = longititude;
+        this.storingCapacity = storingCapacity;
+        this.landingAbility = landingAbility;
+        this.portManager = portManager;
+        this.containers = containers;
+        this.vehicles = vehicles;
+        this.trafficHistory = trafficHistory;
+    }
 }
 
