2f521a7db4e117969578794c6d0b6ed953eacac5
s3986423 <s3986423@rmit.edu.vn>
2023-09-20 12:04:07 +0700

    Update Trip.java
    Create constructor, getter and setter for Trip

--- Trip.java
+++ Trip.java
@@ -7,4 +7,63 @@ public class Trip {
     private Port departurePort;
     private Port arrivalPort;
     private String Status;
+
+    public Trip() {}
+
+    public Trip(Vehicles vehicles, LocalDate departureDate, LocalDate arrivalDate, Port departurePort, Port arrivalPort, String status) {
+        this.vehicles = vehicles;
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+        this.departurePort = departurePort;
+        this.arrivalPort = arrivalPort;
+        Status = status;
+    }
+
+    protected Vehicles getVehicles() {
+        return vehicles;
+    }
+
+    protected void setVehicles(Vehicles vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    protected LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    protected void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    protected LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    protected void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    protected Port getDeparturePort() {
+        return departurePort;
+    }
+
+    protected void setDeparturePort(Port departurePort) {
+        this.departurePort = departurePort;
+    }
+
+    protected Port getArrivalPort() {
+        return arrivalPort;
+    }
+
+    protected void setArrivalPort(Port arrivalPort) {
+        this.arrivalPort = arrivalPort;
+    }
+
+    protected String getStatus() {
+        return Status;
+    }
+
+    protected void setStatus(String status) {
+        Status = status;
+    }
 }
