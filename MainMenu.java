6a25b794f07e8b8e9fe825b5f8105c1a1a1c0615
khoabronze <s3986281@rmit.edu.vn>
2023-09-23 23:50:21 +0700

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class MainMenu {
     public void displayMainMenu(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("COSC2081 GROUP ASSIGNMENT");
         System.out.println("CONTAINER PORT MANAGEMENT SYSTEM");
         System.out.println("Instructor: Mr. Minh Vu & Dr. Phong Ngo");
@@ -9,5 +10,16 @@ public class MainMenu {
         System.out.println("s3975681, Phan Nguyen Phuoc Tien");
         System.out.println("s3986423, Nguyen Huu Quoc Huy" );
         System.out.println("s3974923, Nguyen Tran Tri An");
+        System.out.println("====================================");
+        System.out.println("CONTAINER PORT MANAGEMENT MAIN MENU");
+        System.out.println("Press a number to continue");
+        System.out.println("'1' to go to Port Manager Menu ");
+        System.out.println("'2' to go to System Admin Menu");
+        System.out.println("'3' to exit ");
+        int Menuchoice = scanner.nextInt();
+        switch (Menuchoice){
+            case 1:
+
+        }
     }
 }
